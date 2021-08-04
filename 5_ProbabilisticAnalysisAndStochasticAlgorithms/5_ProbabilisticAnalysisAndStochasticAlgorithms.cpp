21fe14a222c83289b0b22a9a8346bc6e4e8a14f4
JiXiangYu <458682984@qq.com>
2021-08-04 21:35:16 +0800

    * 新增第六章内容

--- 5_ProbabilisticAnalysisAndStochasticAlgorithms/5_ProbabilisticAnalysisAndStochasticAlgorithms.cpp
+++ 5_ProbabilisticAnalysisAndStochasticAlgorithms/5_ProbabilisticAnalysisAndStochasticAlgorithms.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <time.h>
 
 /*
 雇佣问题
@@ -20,8 +21,53 @@ void hireAssistant(int n) {
         //}
     }
 }
+/*
+    对于诸如雇用问题之类的问题，其中假设输入的所有排列等可能出现往往有益，通过概率分析可以指导设计一个随机算法。
+    我们不是假设输入的一个分布，而是设定一个分布。
+    特别地，在算法运行前，先随机地排列应聘者，以加强所有排列都是等可能出现的性质。
+    尽管已经修改了这个算法，我们仍希望雇用一个新的办公助理大约需要lnn次期望值。
+    但是现在我们期望对于所有的输入它都是这种情况，而不是对于一个具有特别分布的输入。
+*/
+void randomizedHireAssistant(int n) {
+    //随机排列候选名单
+    int best = 0;
+    for (int i = 0; i < n; i++) {
+        //面试候选人 i
+        //if(候选人i比候选人best更好){
+            //best = i;
+            //雇佣候选人 i
+        //}
+    }
+}
+/*
+    产生随机排列的一个通常的方法是为数组的每个元素A[i]赋一个随机的优先级P[i]，然后依据优先级对数组A中的元素进行排序。
+    例如，如果初始数组A=(1, 2, 3, 4)，随机选择的优先级P=(36, 3, 62, 19)，
+    则将产生一个数组B=(2, 4, 1, 3)，因为第2个优先级最小，接下来是第4个，然后第1个，最后第3个。
+*/
+void permuteBySorting(int* numList, int length) {
+    int* p = new int[length];
+    srand(time(NULL));
+    for (int i = 0; i < length; i++) {
+        *(p + i) = rand();
+    }
+    int* returnList = new int[length];
 
+    //以p为标准对numList进行排序
+}
+/*
+    产生随机排列的一个更好方法是原址排列给定数组。
+*/
+void randomizeInPlace(int* numList, int length) {
+    for (int i = 0; i < length; i++) {
+        //交换 numList[i] 和 numList[j] //i < j < length  
+    }
+}
 
+// 概率分析和指示器随机变量的进一步使用
+/**
+  * 第一个例子是生日悖论。
+  * 一个屋子里人数必须要达到多少人，才能使其中两人生日相同的机会达到50%?
+  */
 
 int main()
 {
---
+++ 6-HeapSort/6_HeapSort.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+
+/*
+    堆排序
+        与归并排序一样，但不同于插入排序的是，堆排序的时间复杂度是O(nlgn)。
+        而与插入排序相同，但不同于归并排序的是，堆排序同样具有空间原址性:任何时候都只需要常数个额外的元素空间存储临时数据。
+        因此，堆排序是集合了我们目前已经讨论的两种排序算法优点的一种排序算法。
+
+        (二叉)堆是一个数组，它可以被看成一个近似的完全二叉树(见B.5.3节)。
+        树上的每一个结点对应数组中的一个元素。除了最底层外，该树是完全充满的，而且是从左向右填充。
+        表示堆的数组A包括两个属性:A.length(通常)给出数组元素的个数，A.heap-size表示有多少个堆元素存储在该数组中。
+        也就是说，虽然A[1 .. A.length]可能都存有数据，但只有A[1 .. A.heap-size]中存放的是堆的有效元素，这里，0≤A.heap-size≤A.length。
+        树的根结点是A[1]，这样给定一个结点的下标i，我们很容易计算得到它的父结点、左孩子和右孩子的下标:
+        Parent(i)
+            return A[i >> 1];
+
+        Left(i)
+            return A[i << 1];
+
+        Right(i)
+            return A[i << 1 + 1];
+
+    分类：
+        二叉堆可以分为两种形式:最大堆和最小堆。
+        在这两种堆中，结点的值都要满足堆的性质，但一些细节定义则有所差异。
+        在最大堆中，最大堆性质是指除了根以外的所有结点i都要满足:
+            A[PARENT(i)]≥A[i] 也就是说，某个结点的值至多与其父结点一样大。
+        最小堆的组织方式正好相反:最小堆性质是指除了根以外的所有结点主都有:
+            A[PARENT(i)]≤A[i] 最小堆中的最小元素存放在根结点中。
+        
+    在堆排序算法中，使用的是最大堆，最小堆通常用于构造优先队列。
+*/
+
+/*
+    MAX-HEAPIFY是用于维护最大堆性质的重要过程。
+        它的输人为一个数组A和一个下标i。
+        在调用MAX-HEAPIFY的时候，我们假定根结点为LEFT(i)和RIGHT(i)的二叉树都是最大堆,
+        但这时A[i]有可能小于其孩子，这样就违背了最大堆的性质。
+        MAX-HEAPIFY通过让A[i]的值在最大堆中“逐级下降”，从而使得以下标i为根结点的子树重新遵循最大堆的性质。
+*/
+int getLeftIndex(int i) {
+    return (i << 1);
+}
+int getRightIndex(int i) {
+    return (i << 1) + 1;
+}
+void maxHeapify(int* numList, int i, int numListSize) {
+    int leftIndex = getLeftIndex(i);
+    int rightIndex = getRightIndex(i);
+
+    int largestIndex;
+    if (leftIndex <= numListSize && numList[leftIndex] > numList[i])
+        largestIndex = leftIndex;
+    else
+        largestIndex = i;
+    if (rightIndex <= numListSize && numList[rightIndex] > numList[largestIndex])
+        largestIndex = rightIndex;
+
+    if (largestIndex != i) {
+        int exchangeNum = numList[i];
+        numList[i] = numList[largestIndex];
+        numList[largestIndex] = exchangeNum;
+        maxHeapify(numList, largestIndex, numListSize);
+    }
+}
+/*
+    我们可以用自底向上的方法利用过程MAX-HEAPIFY把一个大小为n=A.length的数组A[1 .. n]转换为最大堆。
+    子数组A[n >> 1] + 1..n)中的元素都是树的叶结点。
+    每个叶结点都可以看成只包含一个元素的堆。
+    过程BUILD-MAX-HEAP对树中的其他结点都调用一次MAX-HEAPIFY。
+*/
+void buildMaxHeap(int* numList, int numListSize) {
+    for (int i = numListSize << 1; i > 0; i--) {
+        maxHeapify(numList, i, numListSize);
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
